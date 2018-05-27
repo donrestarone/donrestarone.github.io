6fd231125a217651548dfc29f573ab2848ae830f
donrestarone <shashikejayatunge@gmail.com>
2018-05-27 17:46:38 -0400

    added mobile friendly breakout of the hidden div when clicked

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,25 +1,52 @@
 document.addEventListener("DOMContentLoaded", function(event){
+	// for table interactivity
 	var specialities = document.getElementById("specialties");
 	var partialBoxes = document.getElementsByClassName("box");
-	var counter = 0;
 
-	var looper = setInterval(function(){
-		counter++;
-		for (var i=0; i < partialBoxes.length; i++){
-			aBox = partialBoxes[i]
-			aBox.style.display = 'block';
+	var chartTitles = document.getElementsByClassName("col-sm-3");
+	var instruction = document.getElementById("instruction");
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		var i
+		for (var ii = 0; i < chartTitles.length; i++){
+			chartTitles[ii].addEventListener('click', function(event){
+				for (i= 0; i < partialBoxes.length; i++){
+					partialBoxes[ii].style.display = 'block';
+				}
+			})
 		}
+	}
 
-		if (counter == 2){
-			clearInterval(looper)
-		}
-	}, 850)
+
 
 	setInterval(function(){
-		for (var i=0; i < partialBoxes.length; i++){
-			aBox = partialBoxes[i]
-			aBox.style.display = 'none';
-		}
-	}, 1200)
+		instruction.style.display = 'none'
+	}, 650)
+
+
+
+
+
+
+
+	// var looper = setInterval(function(){
+	// 	counter++;
+	// 	// var aBox
+	// 	for (var i=0; i < partialBoxes.length; i++){
+	// 		var aBox = partialBoxes[i]
+	// 		aBox.style.display = 'block';
+	// 	}
+
+
+	// }, 850)
+
+	// setInterval(function(){
+	// 	for (var i=0; i < partialBoxes.length; i++){
+	// 		aBox = partialBoxes[i]
+	// 		aBox.style.display = 'none';
+	// 	}
+	// 	if (counter == 2){
+	// 		clearInterval(looper)
+	// 	}
+	// }, 1200)
 
 });
